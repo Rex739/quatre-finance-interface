f78afdccd75b6f56ff450190c9e8f7a8ebd374b5
Okeke Christian <christian.okeke24@gmail.com>
2021-11-13 20:52:12 +0000

    Created Presale Tokenomics section

--- pages/presale.js
+++ pages/presale.js
@@ -1,11 +1,15 @@
-import WhitePaper from '../components/whitepaper'
-import RoadMap from '../components/roadMap'
-import PresaleInfo from '../components/presales/presaleInfo'
+import WhitePaper from '../components/whitepaper';
+import RoadMap from '../components/roadMap';
+import PresaleInfo from '../components/presales/presaleInfo';
+import PresaleTokenomics from '../components/presales/presaleTokenomics';
 
 const Presales = ({lightMode }) => {
     return ( 
         <main>
             <PresaleInfo
+                lightMode={lightMode}
+            />
+            <PresaleTokenomics 
                 lightMode={lightMode}
             />
             <RoadMap 

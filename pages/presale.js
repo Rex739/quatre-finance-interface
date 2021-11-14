0ef2827f1e93f150081eb79de13358a9e6389c7b
Okeke Christian <christian.okeke24@gmail.com>
2021-11-14 20:10:50 +0000

    Created the How To Claim section

--- pages/presale.js
+++ pages/presale.js
@@ -3,11 +3,15 @@ import RoadMap from '../components/roadMap';
 import PresaleInfo from '../components/presales/presaleInfo';
 import PresaleTokenomics from '../components/presales/presaleTokenomics';
 import Hero from '../components/presales/hero';
+import HowToClaim from '../components/presales/howToClaim';
 
 const Presales = ({lightMode }) => {
     return ( 
         <main>
             <Hero
+                lightMode={lightMode}
+            />
+            <HowToClaim
                 lightMode={lightMode}
             />
             <PresaleInfo

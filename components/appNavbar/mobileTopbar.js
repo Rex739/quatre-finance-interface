de55d0c0ba53fb9f99be9164e4857e0ac3dc6d6b
user <ojemehprogress@gmail.com>
2021-11-23 13:05:28 +0100

    updated site

--- components/appNavbar/mobileTopbar.js
+++ components/appNavbar/mobileTopbar.js
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 import styles from '../../styles/local/components/navbar.module.css'
-import Image from 'next/image'
 
-const TopBar = ({isClicked, handleClick, toggleMode, lightMode, sound}) => {
+
+const TopBar = ({isClicked, handleClick, lightMode}) => {
     return ( 
         <section className={lightMode ? [styles.mobileNav, styles.lightMode, "list-unstyled"].join(" ") : [styles.mobileNav, styles.darkMode, "list-unstyled"].join(" ")}>
             <ul className="d-flex wrapper list-unstyled my-auto">
                 <li className="my-3">
                     <Link href="/" >
                         <a className="d-flex align-items-center">
-                            <Image src="/images/logo.png" alt="logo" className="logo my-auto" id="logo" width={66} height={20} />
+                            <img src="/images/logo.png" alt="logo" className="logo my-auto" id="logo" width={66} height={20} />
                         </a>
                     </Link>
                 </li>

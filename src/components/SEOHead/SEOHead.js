3bfa5bcf8b1cc0e73c5402f2121af6d85908ca2b
Rex739 <ojemehprogress@gmail.com>
2023-03-13 22:32:04 +0100

    modified SEOHEAD

--- src/components/SEOHead/SEOHead.js
+++ src/components/SEOHead/SEOHead.js
@@ -1,15 +1,8 @@
-import NextHead from 'next/head'
-import React from 'react'
+import NextHead from "next/head"
+import React from "react"
 
-const SEOHead = ({
-  title,
-  description,
-  url,
-  ogImage
-}) => {
-  const joinedTitle = title
-    ? `${title} | Quatre Finance`
-    : "Quatre Finance "
+const SEOHead = ({ title, description, url, ogImage }) => {
+  const joinedTitle = title ? `${title} | Quatre Finance` : "Quatre Finance "
 
   return (
     <NextHead>
@@ -22,10 +15,7 @@ const SEOHead = ({
         name="viewport"
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
       />
-      <meta
-        name="keywords"
-        content="Defi, Protocol Web3"
-      />
+      <meta name="keywords" content="Defi, Protocol Web3" />
       <meta property="og:url" content={url} />
       <meta property="og:title" content={joinedTitle} />
       <meta property="og:description" content={description} />
@@ -39,18 +29,18 @@ const SEOHead = ({
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <link rel="icon" href="/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#061A17" />
+      <meta name="msapplication-TileColor" content="#F9F4F4" />
+      <meta name="theme-color" content="#F87C00" />
     </NextHead>
   )
 }
 
 SEOHead.defaultProps = {
-  title: '',
-  description: "Quatre Finance - World's first ever story-based anime NFT project.",
+  title: "",
+  description:
+    "Quatre Finance - World's first ever story-based anime NFT project.",
   // url: 'https://kingdomofpandaria.com/',
-  ogImage:
-    'images/gallery/1.png'
+  ogImage: "images/gallery/1.png",
 }
 
 export default SEOHead

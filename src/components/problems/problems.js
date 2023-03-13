91e9a2bc3204aee78cc2a95d2ab2326c18a9f627
Rex739 <ojemehprogress@gmail.com>
2023-03-13 19:07:52 +0100

    created problems section created problems data

---
+++ src/components/problems/problems.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SectionHeader from "../sectionHeader"
+
+const Problem = () => {
+  return ( 
+    <section>
+      <SectionHeader 
+        sectionTitle={data.sectionTitle}
+        subTitle={data.subTitle}
+
+      />
+
+    </section>
+   );
+}
+ 
+export default Problem;

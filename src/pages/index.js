54a31b3d4ae674391ed5112501d901c05fe14c8f
Rex739 <ojemehprogress@gmail.com>
2023-03-13 21:46:00 +0100

    removed transparent layer

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import PlayStore from "../components/playstore"
 import Problems from "../components/problems/problems"
 import Solutions from "../components/solutions/solutions"
 // import SEOHead from "../components/SEOHead/SEOHead"
-import TransparenLayer from "../components/transparentLayer"
+// import TransparenLayer from "../components/transparentLayer"
 
 const Home = ({ isClicked }) => {
   return (
     <>
       <main className="bg-green1 relative">
         {/* white transparent layer when navbar toggle button is clicked  */}
-        <TransparenLayer isClicked={isClicked} />
+        {/* <TransparenLayer isClicked={isClicked} /> */}
         <Hero />
         <About />
         <Problems />
